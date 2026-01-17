670f87bdd502de07e5bd4565ba0548d0ab6db86a
Karepann <59456973+Gamja-Kim@users.noreply.github.com>
2026-01-17 14:19:33 +0900

    Update wedding details and groom information

--- config.js
+++ config.js
@@ -3,11 +3,11 @@ export const KAKAOTALK_API_TOKEN = "JavaScript 키 입력";
 export const KAKAOTALK_SHARE_IMAGE =
   "https://cdn.pixabay.com/photo/2014/11/13/17/04/heart-529607_960_720.jpg";
 
-export const WEDDING_DATE = "1970년 01월 01일, 목요일 오전 12시 00분";
-export const WEDDING_LOCATION = "○○○웨딩, ○층 ○○홀";
+export const WEDDING_DATE = "2027년 02월 28일, 일요일 오후 1시 00분";
+export const WEDDING_LOCATION = "더채플 논현, 라포레홀";
 
-export const GROOM_NAME = "○○○";
-export const GROOM_ACCOUNT_NUMBER = "○○은행 ***-***-******";
+export const GROOM_NAME = "김진호";
+export const GROOM_ACCOUNT_NUMBER = "하나은행 173-910325-61107";
 export const GROOM_FATHER_NAME = "○○○";
 export const GROOM_FATHER_ACCOUNT_NUMBER = "○○은행 ***-***-******";
 export const GROOM_MOTHER_NAME = "○○○";
